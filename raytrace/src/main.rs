17160eee60c6a1f6c5cbd937e60e73319783df0c
Florian Schmiderer <florian.schmiderer@yahoo.de>
2019-04-02 20:41:43 +0200

    raycasting ray directions

--- raytrace/src/main.rs
+++ raytrace/src/main.rs
@@ -5,7 +5,9 @@ extern crate ncollide3d;
 mod gof;
 
 use mini_gl_fb::{BufferFormat, Config};
-use nalgebra::{Unit, Vector3};
+use nalgebra::{Isometry3, Point2, Point3, Unit, Vector3};
+use ncollide3d::query::{Ray, RayCast};
+use ncollide3d::shape::Cuboid;
 
 #[repr(C)]
 struct Color {
@@ -23,6 +25,40 @@ impl Color {
 struct Ray {
     origin: Vector3<f32>,
     dir: Unit<Vector3<f32>>,
+}
+
+struct Camera {
+    up: Unit<Vector3<f32>>,
+    right: Unit<Vector3<f32>>,
+    forward: Unit<Vector3<f32>>,
+}
+
+impl Camera {
+    fn new_with_forward_dir(forward: Unit<Vector3<f32>>) -> Self {
+        let world_up: Unit<Vector3<f32>> = Vector3::y_axis();
+        let right: Unit<Vector3<f32>> = Unit::new_normalize(forward.cross(&world_up));
+        let up: Unit<Vector3<f32>> = Unit::new_normalize(right.cross(&forward));
+        Camera { up, right, forward }
+    }
+
+    /// normalised_screen_pixel: should be in range from -1 to +1. One of the two components may be
+    /// greater to adjust for Aspect ratio
+    fn calc_ray_dir(
+        &self,
+        normalised_screen_pixel: Point2<f32>,
+        field_of_view_scaling: f32,
+    ) -> Unit<Vector3<f32>> {
+        // we treat camera origin as 0, we are only interested in direction so it does not matter
+        // if the two points whicht define that direction are translated by the same amount
+
+        let projection_plane_origin = Point3::from(*self.forward.as_ref()); // + Point3::origin(); // Point3::from(self.forward);
+        let point_on_projection_plane = projection_plane_origin
+            + normalised_screen_pixel.x * self.right.as_ref() * field_of_view_scaling
+            + normalised_screen_pixel.y * self.up.as_ref() * field_of_view_scaling;
+
+        // we can just use the normalized point as the origin is 0.
+        Unit::new_normalize(point_on_projection_plane.coords)
+    }
 }
 
 const ROWS: i32 = 800;
@@ -48,9 +84,44 @@ fn main() {
         Color::new(0, 0, 0),
         Color::new(0, 0, 255),
     ];
+    let mut vec = vec![0; PIXELS as usize];
     let field_of_view_deg: f32 = 90.;
     let field_of_view_rad = f32::to_radians(field_of_view_deg);
     let field_of_view_scaling = f32::tan(field_of_view_rad / 2.0f32);
+    let aspect_ratio_scaling = COLLS_F / ROWS_F;
+    let cam = Camera::new_with_forward_dir(Vector3::z_axis());
+    let cuoid = Cuboid::new(Vector3::new(1.0, 2.0, 1.0));
+    {
+        let mut x = 0;
+        let mut y = 0;
+        for elem in vec {
+            let norm_x = x as f32 * COLLS_INV_F;
+            let norm_y = y as f32 * ROWS_INV_F;
+
+            let screen_x = (norm_x * 2.0) - 1.0;
+            let screen_y = (norm_y * 2.0) - 1.0;
+            let normalised_screen_pixel = Point2::new(screen_x * aspect_ratio_scaling, screen_y);
+            let ray_dir = cam.calc_ray_dir(normalised_screen_pixel, field_of_view_scaling);
+
+            let ray = Ray::new(Point3::new(0.0, 0.0, 0.0), ray_dir);
+
+            let raycast_result = cuoid.toi_and_normal_with_ray(&Isometry3::identity(), &ray, true);
+
+            let normal_as_color = raycast_result. * 128.0 + Vector3::repeat(127.0f32);
+            elem = Color::new(
+                normal_as_color.x as u8,
+                normal_as_color.y as u8,
+                normal_as_color.z as u8,
+            );
+            // Do Raytrace
+            x += 1;
+            if (x == COLLS) {
+                x = 0;
+                y += 1;
+            }
+        }
+        assert!(y == ROWS && x == 0);
+    }
 
     fb.update_buffer(&buffer);
     fb.persist();
