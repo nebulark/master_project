17e2b235ff10b261c296fa18ed0411964f0e3752
Florian Schmiderer <florian.schmiderer@yahoo.de>
2019-05-22 20:11:45 +0200

    starting with vulkan

--- master_project/vulkan_triangle/main.cpp
+++ master_project/vulkan_triangle/main.cpp
@@ -3,10 +3,79 @@
 
 #include "pch.h"
 
+
+
+#include "common/Deleters.hpp"
+
+vk::UniqueInstance create_vulkan_instance()
+{
+	vk::ApplicationInfo application_info("Hello Triangle", 1, "My Engine", 1, VK_API_VERSION_1_1);
+
+
+	const char* enabledLayers[] = 
+	{
+		// Enable standard validation layer to find as much errors as possible!
+		"VK_LAYER_KHRONOS_validation" 
+	};
+
+	
+	const char* enabledExtensions[] =
+	{
+		VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
+	};
+
+	vk::InstanceCreateInfo instanceCreateInfo({}, &application_info,
+		std::size(enabledLayers), enabledLayers,
+		std::size(enabledExtensions), enabledExtensions);
+
+
+	vk::UniqueInstance instance = vk::createInstanceUnique(instanceCreateInfo);
+
+		
+	
+	return instance;
+}
+
+struct ReportCallbacks
+{
+	PFN_vkCreateDebugReportCallbackEXT  pfnVkCreateDebugReportCallbackEXT;
+	PFN_vkDestroyDebugReportCallbackEXT pfnVkDestroyDebugReportCallbackEXT;
+};
+
+ReportCallbacks LoadReportCallbacks(vk::Instance* instance)
+{
+	ReportCallbacks result;
+	result.pfnVkCreateDebugReportCallbackEXT = reinterpret_cast<PFN_vkCreateDebugReportCallbackEXT>(instance->getProcAddr("vkCreateDebugReportCallbackEXT"));
+	result.pfnVkDestroyDebugReportCallbackEXT = reinterpret_cast<PFN_vkDestroyDebugReportCallbackEXT>(instance->getProcAddr("vkDestroyDebugReportCallbackEXT"));
+}
+
+
+
+
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	SDL_Init(SDL_INIT_EVERYTHING);
+	{
+		WindowPtr window{
+				SDL_CreateWindow(
+				"An SDL2 window",                  // window title
+				SDL_WINDOWPOS_UNDEFINED,           // initial x position
+				SDL_WINDOWPOS_UNDEFINED,           // initial y position
+				1920,                               // width, in pixels
+				1080,                               // height, in pixels
+				SDL_WINDOW_OPENGL                 // flags - see below
+			)
+		};
+
+		
+
+	
+	}
+	SDL_Quit();
+		
+
 	return 0;
+	
 }
 
 
--- master_project/vulkan_triangle/pch.h
+++ master_project/vulkan_triangle/pch.h
@@ -1,3 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <cassert>
+#include <cstdint>
+#include <chrono>
+
+#include "SDL.h"
+#include "glm.hpp"
+
+#pragma warning(push, 0)   
+#include <vulkan/vulkan.hpp>
+#pragma warning(pop)
