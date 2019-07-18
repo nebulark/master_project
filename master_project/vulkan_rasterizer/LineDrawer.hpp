f0a4cffc5d01cf91c349185f671c80fae00ad458
Florian Schmiderer <florian.schmiderer@yahoo.de>
2019-07-18 15:29:33 +0200

    added convenience constructor for lines

--- master_project/vulkan_rasterizer/LineDrawer.hpp
+++ master_project/vulkan_rasterizer/LineDrawer.hpp
@@ -10,6 +10,19 @@ struct Line
 	glm::vec4 pointB;
 	glm::vec4 colorA;
 	glm::vec4 colorB;
+
+	Line() = default;
+
+	constexpr Line(
+		const glm::vec4& pointA,
+		const glm::vec4& pointB,
+		const glm::vec4& colorA = glm::vec4(1.f,0.f,0.f,1.f),
+		const glm::vec4& colorB = glm::vec4(0.f,0.f,1.f,1.f))
+		: pointA(pointA)
+		, pointB(pointB)
+		, colorA(colorA)
+		, colorB(colorB)
+	{}
 };
 
 class LineDrawer
